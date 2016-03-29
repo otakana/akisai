1e676a82329a23491162b058234eebc0732bc290
kannsukeH <kansukebano@gmail.com>
2016-03-29 23:42:10 +0900

    test2
    test2

--- index.js
+++ index.js
@@ -70,3 +70,13 @@
             }, 1000);
         });
     });
+
+    function initialize() {
+        var latlng = new google.maps.LatLng(35.388429,139.427324);
+        var myOptions = {
+            zoom: 18, /*拡大比率*/
+            center: latlng, /*表示枠内の中心点*/
+            mapTypeId: google.maps.MapTypeId.ROADMAP/*表示タイプの指定*/
+        };
+        var map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+    }
