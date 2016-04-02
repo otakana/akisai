d3b7da3cf2e28cc91b6880921afabd1a9b3b50f5
kannsukeH <kansukebano@gmail.com>
2016-04-02 20:23:15 +0900

    delete fadein out

--- index.js
+++ index.js
@@ -12,9 +12,9 @@
             $('#left_side').animate({width:"14%"}, "slow" );
             $('#right_side').animate({width:"86%"}, "slow" );
         });
-        $('.smart_menu').click(function(){
+        /*$('.smart_menu').click(function(){
             $('body').fadeTo(500,0);
-        });
+        });*/
         $("#about").mouseover(function(){
             position = $("#smoothplay1").offset().top;
             link();
@@ -69,13 +69,12 @@
         });
     });
     $(window).load(function(){
-        //alert("b1");
-        $('body').hide();
-        $('body').fadeIn(0);
+        //$('body').hide();
+        //$('body').fadeIn(0);
         $('.smart_menu').click(function(){
             var url = $(this).children('a').attr('href');
             if (url != ''){
-                $('body').fadeOut(1000);
+                //$('body').fadeOut(1000);
                 setTimeout(function(){
                     location.href = url;
                 }, 1000);
